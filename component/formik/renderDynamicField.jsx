d0cffeeafb113ac2ac659a6d0d6e20d00ad4d9cb
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2021-05-18 10:13:14 +0530

    update purchase

--- component/formik/renderDynamicField.jsx
+++ component/formik/renderDynamicField.jsx
@@ -1,163 +1,179 @@
-import React from 'react';
-import Textbox from './textbox';
-import Autocomplete from './autocomplete';
-import SimpleReactValidator from 'simple-react-validator';
-import { useFormik } from 'formik';
-import { Card, CardContent, CardHeader, Icon, makeStyles, Table, TableRow, TableBody, TableCell, TableHead } from '@material-ui/core';
-
+import React from "react";
+import Textbox from "./textbox";
+import Autocomplete from "./autocomplete";
+import SimpleReactValidator from "simple-react-validator";
+import { useFormik } from "formik";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Icon,
+  makeStyles,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,  
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(0, 0, 2),
-    },
-    cancel: {
-        margin: theme.spacing(0, 2, 2),
-    },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(0, 0, 2),
+  },
+  cancel: {
+    margin: theme.spacing(0, 2, 2),
+  },
 }));
 
-
 const Index = (props) => {
-    const { form, name, label, value } = props
-    const classes = useStyles();
+  const { form, name, label, value } = props;
+  const classes = useStyles();
 
-    const addOption = () => {
-        const value = formik.values[name]
-        let formData = []
+  const addOption = () => {
+    const value = formik.values[name];
+    let formData = [];
 
-        if (form) {
-            form.forEach(element => {
-                formData[element.name] = ''
-            });
-        }
-
-        value.push(Object.assign({}, formData))
-        formik.setFieldValue([name], value)
+    if (form) {
+      form.forEach((element) => {
+        formData[element.name] = "";
+      });
     }
 
-    const removeOption = (index) => {
-        if (index > -1) {
-            const value = formik.values[name]
-            value.splice(index, 1)
-            formik.setFieldValue([name], value)
-        }
+    value.push(Object.assign({}, formData));
+    formik.setFieldValue([name], value);
+  };
+
+  const removeOption = (index) => {
+    if (index > -1) {
+      const value = formik.values[name];
+      value.splice(index, 1);
+      formik.setFieldValue([name], value);
     }
+  };
 
-    const simpleValidator = React.useRef(new SimpleReactValidator({
-        element: message => message,
-    }))
+  const simpleValidator = React.useRef(
+    new SimpleReactValidator({
+      element: (message) => message,
+    })
+  );
 
-    const formik = useFormik({
-        initialValues: {
-            [name]: value
-        },
-        validate: (values) => {
-            const errors = {};
-            // const { form } = props
-            // if (form) {
-            //     form.forEach(element => {
-            //         const value = values[element.name] ? values[element.name] : ""
-            //         const validation = element.validation ? element.validation : ""
-            //         if (validation) {
-            //             simpleValidator.current.showMessages()
-            //             const fieldError = simpleValidator.current.message(element.name, value, validation)
-            //             if (fieldError) {
-            //                 errors[element.name] = fieldError
-            //             }
-            //         }
-            //     });
-            // }
-            // simpleValidator.current.showMessages()
-            return errors;
-        },
-        onSubmit: async (values) => {
-            props.onChange(values[name])
-        },
-    });
+  const formik = useFormik({
+    initialValues: {
+      [name]: value,
+    },
+    validate: (values) => {
+      const errors = {};
+      // const { form } = props
+      // if (form) {
+      //     form.forEach(element => {
+      //         const value = values[element.name] ? values[element.name] : ""
+      //         const validation = element.validation ? element.validation : ""
+      //         if (validation) {
+      //             simpleValidator.current.showMessages()
+      //             const fieldError = simpleValidator.current.message(element.name, value, validation)
+      //             if (fieldError) {
+      //                 errors[element.name] = fieldError
+      //             }
+      //         }
+      //     });
+      // }
+      // simpleValidator.current.showMessages()
+      return errors;
+    },
+    onSubmit: async (values) => {
+      props.onChange(values[name]);
+    },
+  });
 
-    React.useEffect(() => {
-        formik.setValues({ [name]: value })
-        formik.submitForm()
-    }, [value])
+  React.useEffect(() => {
+    formik.setValues({ [name]: value });
+    formik.submitForm();
+  }, [value]);
 
-    return (
-        <form className={classes.form} noValidate>
-            <Card>
-                <CardHeader titleTypographyProps={{ variant: 'h6', align: 'center' }} title={label} action={<Icon onClick={() => addOption()}>add_circle_outline</Icon>} />
-                <CardContent>
+  return (
+    <form className={classes.form} noValidate>
+      <Card>
+        <CardHeader
+          titleTypographyProps={{ variant: "h6", align: "center" }}
+          title={label}
+          action={<Icon onClick={() => addOption()}>add_circle_outline</Icon>}
+        />
+        <CardContent>
+          <Table>
+            <TableBody>
+              {formik.values &&
+                formik.values[name] &&
+                formik.values[name].map((option, index) => {
+                  return (
+                    <TableRow>
+                      <TableCell>
+                        {form &&
+                          form.map((formData) => {
+                            const value =
+                              formik.values[name][index][formData.name];
 
-                    <Table>
-                        <TableBody>
-                            {formik.values && formik.values[name] && formik.values[name].map((option, index) => {
-
+                            switch (formData.type) {
+                              case "autocomplete":
                                 return (
-                                    <TableRow>
-                                        <TableCell>
-                                            {form && form.map((formData) => {
+                                  <Autocomplete
+                                    required={formData.required}
+                                    disabled={formData.disabled}
+                                    fullWidth={formData.fullWidth}
+                                    label={formData.label}
+                                    url={formData.url}
+                                    getOptionLabel={formData.getOptionLabel}
+                                    getOptionValue={formData.getOptionValue}
+                                    value={value}
+                                    onChange={(e) => {
+                                      let data = formik.values;
+                                      data[name][index][formData.name] = e;
+                                      formik.setValues(data);
+                                      formik.handleSubmit();
+                                    }}
+                                  />
+                                );
 
-                                                const value = formik.values[name][index][formData.name]
+                              default:
+                                return (
+                                  <Textbox
+                                    required={formData.required}
+                                    multiline={formData.multiline}
+                                    disabled={formData.disabled}
+                                    fullWidth={formData.fullWidth}
+                                    label={formData.label}
+                                    type={formData.type}
+                                    icon={formData.icon}
+                                    value={value || ""}
+                                    onChange={(e) => {
+                                      let data = formik.values;
+                                      data[name][index][formData.name] =
+                                        e.target.value;
+                                      formik.setValues(data);
+                                      formik.handleSubmit();
+                                    }}
+                                  />
+                                );
+                            }
+                          })}
+                      </TableCell>
 
-                                                switch (formData.type) {
-                                                    case 'autocomplete':
-                                                        return (
-                                                            <Autocomplete
-                                                                required={formData.required}
-                                                                disabled={formData.disabled}
-                                                                fullWidth={formData.fullWidth}
-                                                                label={formData.label}
-                                                                url={formData.url}
-                                                                getOptionLabel={formData.getOptionLabel}
-                                                                getOptionValue={formData.getOptionValue}
-                                                                value={value}
-                                                                onChange={(e) => {
-                                                                    let data = formik.values
-                                                                    data[name][index][formData.name] = e
-                                                                    formik.setValues(data)
-                                                                    formik.handleSubmit()
-                                                                }}
-                                                            />
-                                                        )
+                      <TableCell>
+                        <Icon onClick={() => removeOption(index)}>
+                          remove_circle_outline
+                        </Icon>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </form>
+  );
+};
 
-                                                    default:
-                                                        return (
-                                                            <Textbox
-                                                                required={formData.required}
-                                                                multiline={formData.multiline}
-                                                                disabled={formData.disabled}
-                                                                fullWidth={formData.fullWidth}
-                                                                label={formData.label}
-                                                                type={formData.type}
-                                                                icon={formData.icon}
-                                                                value={value || ""}
-                                                                onChange={(e) => {
-                                                                    let data = formik.values
-                                                                    data[name][index][formData.name] = e.target.value
-                                                                    formik.setValues(data)
-                                                                    formik.handleSubmit()
-                                                                }}
-                                                            />
-                                                        )
-                                                }
-                                            })}
-                                        </TableCell>
-
-                                        <TableCell>
-                                            <Icon onClick={() => removeOption(index)}>remove_circle_outline</Icon>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })}
-
-                        </TableBody>
-                    </Table>
-
-                </CardContent>
-            </Card>
-        </form>
-    )
-}
-
-export default Index
+export default Index;
